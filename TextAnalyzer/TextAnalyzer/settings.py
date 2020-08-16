f4adb718e735d66d9933eb9bef1a7d768332a8dd
Dishant <25dishant@gmail.com>
2020-08-16 13:51:31 +0530

    removed print

--- TextAnalyzer/TextAnalyzer/settings.py
+++ TextAnalyzer/TextAnalyzer/settings.py
@@ -21,9 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('TEXT_ANALYZER_APP')
-print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
